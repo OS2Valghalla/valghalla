7c75a3b1dcc67891083f0c3f2bffdfcb6fac12f2
Stanislav Kutasevits <stan@bellcom.dk>
2021-08-10 14:28:18 +0300

    Update core 7.73 -> 7.82

---
+++ public_html/profiles/valhalla/deploy_internal.php
@@ -0,0 +1,49 @@
+<?php
+
+// Checkboxes
+variable_set('valghalla_volunteer_validator_enable', 1);
+variable_set('valghalla_volunteer_validator_get_address', 1);
+variable_set('valghalla_volunteer_validator_get_name', 1);
+variable_set('valghalla_volunteer_validator_get_subscription_status', 1);
+
+variable_set('vcv_person_lookup_extended', 1);
+
+variable_set('serviceplatformen', 1);
+
+variable_set('vcv_person_lookup_extended_serviceagreementuuid', '9382fede-45e9-411c-b050-af9a20ac0bb8');
+variable_set('vcv_person_lookup_extended_serviceuuid', 'e6be2436-bf35-4df2-83fe-925142825dc2');
+variable_set('vcv_person_lookup_extended_wsdl', '/Users/stan/Development/AcquiaSites/Valghalla/valg-brondby.bellcom.dk/public_html/PersonBaseDataExtended/wsdl/token/PersonBaseDataExtendedService.wsdl');
+variable_set('vcv_person_lookup_extended_location', 'https://prod.serviceplatformen.dk/service/CPR/PersonBaseDataExtended/4');
+
+variable_set('vcv_person_lookup_extended_usersystemuuid', '8a0aa317-6805-427a-acd5-78f2af15a3c9');
+variable_set('vcv_person_lookup_extended_useruuid', 'ba916990-8d8e-4f43-adf7-042d154ec4f3');
+variable_set('vcv_person_lookup_extended_accountinginfo', 'Valghalla');
+variable_set('vcv_person_lookup_extended_certfile', '/Users/stan/Development/Documents and files/Valghalla/certificates/certfile_live.pem');
+
+variable_set('vcv_serviceplatformen_serviceagreementuuid', 'd3d9d041-f3d4-4fa8-94f6-6d4cc0042304');
+variable_set('vcv_serviceplatformen_serviceuuid', 'd84f1ac8-76ca-11e3-abab-138252136bdf');
+variable_set('vcv_serviceplatformen_wsdl', 'http://fredericia.adm.valghalla.dk/profiles/valhalla/modules/valghalla/modules/valghalla_volunteers/modules/validator/modules/vcv_serviceplatformen/CPRBasicInformationService/CPRBasicInformationService.wsdl');
+variable_set('vcv_serviceplatformen_location', 'https://prod.serviceplatformen.dk/service/CPRBasicInformation/CPRBasicInformation/1');
+
+variable_set('vcv_serviceplatformen_adrsog_serviceagreementuuid', '9382fede-45e9-411c-b050-af9a20ac0bb8');
+variable_set('vcv_serviceplatformen_adrsog_serviceuuid', '6538f113-b1e6-4b21-8fe1-28b7bf78a1bd');
+variable_set('vcv_serviceplatformen_adrsog_location', 'https://prod.serviceplatformen.dk/service/CPR/PersonBaseDataExtended/4');
+
+variable_set('vcv_serviceplatformen_usersystemuuid', '8a0aa317-6805-427a-acd5-78f2af15a3c9');
+variable_set('vcv_serviceplatformen_useruuid', 'd3d6ab10-c915-401c-a186-517e5fb0c8dd');
+variable_set('vcv_serviceplatformen_accountinginfo', 'Valghalla');
+variable_set('vcv_serviceplatformen_certfile', '/Users/stan/Development/Documents and files/Valghalla/certificates/certfile_live.pem');
+
+// Files.
+variable_set('file_public_path', 'sites/valg-brondby.bellcom.dk/files');
+variable_set('file_private_path', 'sites/valg-brondby.bellcom.dk/private/files');
+variable_set('file_temporary_path', '/tmp');
+
+// Interna server.
+variable_set('valghalla_internal_server_failed_sync_report_mail', 'stan@bellcom.ee');
+variable_set('valghalla_internal_server_enable', 1);
+variable_set('valghalla_external_server_endpoint','http://valg-brondby-externalbellcomdk.docksal/rest');
+variable_set('valghalla_external_server_user','admin');
+//variable_set('valghalla_external_server_password','5TGhg3nt00');
+variable_set('valghalla_external_server_password','K9ig3nt00');
+variable_set('valghalla_external_server_hash_salt','OJUExV1QZSB2pnoB76prvD4fhSLJ8vQWEmVke-maczs');
